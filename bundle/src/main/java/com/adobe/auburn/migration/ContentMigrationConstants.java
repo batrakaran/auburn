a222e2e9c4a3e9b8b1330c2433177ba882694b20
Kiran Murugulla <murugull@adobe.com>
2016-04-13 13:01:52 -0700

    Adds path related contants

--- bundle/src/main/java/com/adobe/auburn/migration/ContentMigrationConstants.java
+++ bundle/src/main/java/com/adobe/auburn/migration/ContentMigrationConstants.java
@@ -1,5 +1,8 @@
 package com.adobe.auburn.migration;
 
 public class ContentMigrationConstants {
+private static final String ASSET_MIGRATED_ROOT = "/content/dam/auburn/migrated";
+private static final String ASSET_PROCESSING_ROOT = "/content/dam/auburn";
+private static final String LEAF_NODE_DATE_FORMAT = "Hms";
 
 }
