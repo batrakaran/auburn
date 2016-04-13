1b1b1dedcf5ae037e24528daa587cfd4533c362c
Karan Batra <batra@adobe.com>
2016-04-13 16:10:07 -0400

    stubs for job and scheduler

--- bundle/src/main/java/com/adobe/auburn/migration/job/MigrateAssetJob.java
+++ bundle/src/main/java/com/adobe/auburn/migration/job/MigrateAssetJob.java
@@ -1,5 +1,99 @@
 package com.adobe.auburn.migration.job;
 
-public class MigrateAssetJob {
+import org.apache.commons.lang3.StringUtils;
+import org.apache.felix.scr.annotations.*;
+import org.apache.sling.api.SlingConstants;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.event.jobs.Job;
+import org.apache.sling.event.jobs.consumer.JobConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component(
+        label = "Migrate Asset Job - Sling Job Consumer",
+        description = "Migrate Asset Job  - Sling Job Consumer",
+        // One of the few cases where immediate = true; this is so the Event Listener starts listening immediately
+        immediate = true
+)
+@Properties({
+        @Property(
+                label = "Job Topics",
+                value = {"com/adobe/auburn/asset-migration-job"},
+                description = "[Required] Job Topics this job consumer will to respond to.",
+                name = JobConsumer.PROPERTY_TOPICS,
+                propertyPrivate = true
+        )
+})
+@Service
+public class MigrateAssetJob implements JobConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(MigrateAssetJob.class);
+
+    @Reference
+    ResourceResolverFactory resourceResolverFactory;
+
+
+    @Override
+    public JobResult process(final Job job) {
+
+        // This is the Job's process method where the work will be
+
+        // Jobs status is persisted in the JCR under /var/eventing so the management
+        // of Jobs is NOT a wholly "in-memory" operations.
+
+        // If you have guaranteed VERY FAST processing, it may be better to tie into an event
+
+        // For information on all the data tied to the Job object
+        // > http://sling.apache.org/apidocs/sling7/org/apache/sling/event/jobs/Job.html
+        Map<String, Object> authenticationInfo = new HashMap<>();
+        authenticationInfo.put(ResourceResolverFactory.SUBSERVICE, null);
+
+        ResourceResolver resourceResolver = null;
+        try {
+            resourceResolver = resourceResolverFactory.getServiceResourceResolver(authenticationInfo);
+            log.debug(resourceResolver.getUserID());
+            String path = (String) job.getProperty(SlingConstants.PROPERTY_PATH);
+           // String pageName = (String) job.getProperty(Constants.PN_NAME);
+           // String template = (String) job.getProperty(Constants.PN_TEMPLATE);
+           // String pageTitle = (String) job.getProperty(Constants.PN_TITLE);
+
+
+            if(resourceResolver != null && StringUtils.isNotBlank(path)){
+
+                log.debug(" path in job {}", path);
+
+                Resource parentResource = resourceResolver.getResource(path);
+
+                //TODO... stuff goes here
+
+            }
+
+
+            } catch (LoginException e) {
+            e.printStackTrace();
+            } finally {
+            if (resourceResolver != null && resourceResolver.isLive()) {
+                resourceResolver.close();
+            }
+        }
+
+
+        /**
+         * Return the proper JobResult based on the work done...
+         *
+         * > OK : Processed successfully
+         * > FAILED: Processed unsuccessfully and reschedule
+         * > CANCEL: Processed unsuccessfully and do NOT reschedule
+         * > ASYNC: Process through the JobConsumer.AsyncHandler interface
+         */
+        return JobResult.OK;
+    }
 
 }
--- bundle/src/main/java/com/adobe/auburn/migration/scheduler/AssetMigrationScheduler.java
+++ bundle/src/main/java/com/adobe/auburn/migration/scheduler/AssetMigrationScheduler.java
@@ -1,5 +1,100 @@
 package com.adobe.auburn.migration.scheduler;
 
-public class AssetMigrationScheduler {
 
+        import org.apache.felix.scr.annotations.*;
+        import org.apache.sling.api.SlingConstants;
+        import org.apache.sling.api.resource.LoginException;
+        import org.apache.sling.api.resource.ResourceResolver;
+        import org.apache.sling.api.resource.ResourceResolverFactory;
+        import org.apache.sling.discovery.TopologyEvent;
+        import org.apache.sling.discovery.TopologyEventListener;
+        import org.apache.sling.event.jobs.JobManager;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+@Component(
+        label = "ACS AEM Samples - Cluster Aware Scheduled Service",
+        description = "Sample scheduled service using CQ 5.6/AEM6 TopologyEventListener method",
+        immediate = true // Load immediately
+)
+
+@Properties({
+        @Property(
+                label = "Cron expression defining when this Scheduled Service will run",
+                description = "[every minute = 0 * * * * ?] Visit www.cronmaker.com to generate cron expressions.",
+                name = "scheduler.expression",
+                value = "0 1 0 ? * *"
+        ),
+        @Property(
+                label = "Allow concurrent executions",
+                description = "Allow concurrent executions of this Scheduled Service. This is almost always false.",
+                name = "scheduler.concurrent",
+                propertyPrivate = true,
+                boolValue = false
+        )
+})
+
+@Service
+public class AssetMigrationScheduler implements Runnable, TopologyEventListener {
+    private final Logger log = LoggerFactory.getLogger(AssetMigrationScheduler.class);
+
+    private static final String JOB_NAME = "com/adobe/auburn/asset-migration-job";
+
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    @Reference
+    private JobManager jobManager;
+
+    private boolean isLeader = false;
+
+    @Override
+    public void run() {
+        // Scheduled services that do not have to be cluster aware do not need
+        // to implement this check OR extend TopologyEventListener
+        if (!isLeader) {
+            return;
+        }
+
+        // Scheduled service logic, only run on the Master
+        ResourceResolver adminResourceResolver = null;
+        try {
+            // Be careful not to leak the adminResourceResolver
+            adminResourceResolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+
+            // execute your scheduled service logic here ...
+
+            log.debug(" CreateBrand handleEvent() {}");
+            final Map<String, Object> payload = new HashMap<>();
+            payload.put(SlingConstants.PROPERTY_PATH, "");
+           // payload.put(Constants.PN_TEMPLATE, "");
+           // payload.put(Constants.PN_NAME, "");
+           // payload.put(Constants.PN_TITLE, "");
+
+            // There must be a JobConsumer registered for this Topic
+            jobManager.addJob(JOB_NAME, payload);
+
+
+        } catch (LoginException e) {
+            log.error("Error obtaining the admin resource resolver.", e);
+        } finally {
+            // ALWAYS close resolvers you open
+            if (adminResourceResolver != null) {
+                adminResourceResolver.close();
+            }
+        }
+    }
+
+    /** Topology Aware Methods **/
+
+    @Override
+    public void handleTopologyEvent(final TopologyEvent event) {
+        if (event.getType() == TopologyEvent.Type.TOPOLOGY_CHANGED
+                || event.getType() == TopologyEvent.Type.TOPOLOGY_INIT) {
+            this.isLeader = event.getNewView().getLocalInstance().isLeader();
+        }
+    }
 }
