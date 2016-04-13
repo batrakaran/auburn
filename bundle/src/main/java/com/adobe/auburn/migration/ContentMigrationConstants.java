0b8d4edbcbfdefca38e9d7b61b9647299cde954d
Kiran Murugulla <murugull@adobe.com>
2016-04-13 13:08:30 -0700

    Adds job topic related constants

--- bundle/src/main/java/com/adobe/auburn/migration/ContentMigrationConstants.java
+++ bundle/src/main/java/com/adobe/auburn/migration/ContentMigrationConstants.java
@@ -4,5 +4,6 @@ public class ContentMigrationConstants {
 private static final String ASSET_MIGRATED_ROOT = "/content/dam/auburn/migrated";
 private static final String ASSET_PROCESSING_ROOT = "/content/dam/auburn";
 private static final String LEAF_NODE_DATE_FORMAT = "Hms";
-
+private static final String ASSET_MIGRATION_JOB_TOPIC = "com.adobe.auburn.asset.migration";
+private static final String ASSET_PROCESSING_JOB_TOPIC = "com.adobe.auburn.asset.processing";
 }
